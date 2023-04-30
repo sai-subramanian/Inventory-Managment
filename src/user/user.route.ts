bc4575febb2f9d54e000e837ac644bb25dbc1c5e
sai-subramanian <saiss2003@gmail.com>
2023-05-01 00:52:17 +0530

    fixed route for saving data

--- src/user/user.route.ts
+++ src/user/user.route.ts
@@ -1,16 +1,42 @@
 import {Response , Request , Router} from "express";
 import {User} from "../entity/User";
 import { QueryBuilder } from "typeorm";
+import { AppDataSource } from "../data-source";
 
 const router = Router();
+const userRepository = AppDataSource.getRepository(User);
 router.get("/h", (req:Request,res:Response)=>{
     res.send("hey you found me ");
     console.log("reached");
-    });
-
-router.post("/new", (req:Request,res:Response)=>{
-
 });
+
+// to handle new users
+router.post("/new", async (req:Request,res:Response)=>{
+const {name,email,password,age} = req.body;
+const newuser:User = await new User;
+
+newuser.age = age;
+newuser.email = email;
+newuser.password = password;
+newuser.firstName = name;
+
+await userRepository.save(newuser);
+return res
+.status(200)
+.json({message:"new user created!!"})
+});
+
+
+//to handle the updation of existing users
+/*
+router.put("/update", (req:Request,res:Response)=>{
+    const uid = req.params;
+    const {name,email,password,age} = req.body;
+    const newuser = new User;
+    
+   
+});
+  */  
 
 
 module.exports = router;
