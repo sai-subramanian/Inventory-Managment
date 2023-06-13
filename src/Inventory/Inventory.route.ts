f04a9c8cc3ef0696309508076ec11c2477076701
sai-subramanian <saiss2003@gmail.com>
2023-06-14 01:27:34 +0530

    added updation and deletion routes for Inventory and tested

--- src/Inventory/Inventory.route.ts
+++ src/Inventory/Inventory.route.ts
@@ -4,6 +4,7 @@ import {Inventory} from "../entity/Inventory"
 import { InventoryDto } from "./Inventory.dto";
 import { checkjwt ,ARequest} from "../middleware/auth.middleware";
 import { User } from "../entity/User";
+import { pid } from "process";
 
 const router = Router();
 const InventoryRepository = AppDataSource.getRepository(Inventory);
@@ -19,7 +20,7 @@ router.get("/h", (req:Request,res:Response)=>{
     Inventory NO
 
     **jwt token
- */
+ *///vulnerbility if Inventory Number is not mentioned it will show the full table i.e all invetory of that user
 router.get("/",checkjwt,async (req:ARequest,res:Response)=>{
     
     try{
@@ -45,7 +46,7 @@ router.get("/",checkjwt,async (req:ARequest,res:Response)=>{
  });
 
 
- //to add a new product 
+ //to add a new product in a Particular Inventory No
 /* Req Body: (if a new inventory no is created manage from frontend and just pass the new no here)
     inventory no
     Product Name
@@ -96,7 +97,114 @@ router.post("/addNew",checkjwt,async (req:ARequest,res:Response)=>{
 });
 
 
+// add more quantity to a aldready registred product in a particulart Inventory No
+/* Request body:
+    Product name
+    Product quantity
+    Inventory No
 
+**jwt token (in header)
+*/
+router.put("/add",checkjwt,async (req:ARequest,res:Response)=>{
+    try{
+        const body:InventoryDto = req.body;
+        const user:User = await Userrepository.findOne({where:{id:req.user.id}});
+    
+        //checking if the user exists  
+        if(!user){
+            return res.status(400).json({message:"user not found!!"});
+        }
+    
+        const product:Inventory = await InventoryRepository.findOne({where:{UserId:user.id,ProductName:body.ProductName,InventoryNumber:body.InventoryNumber}});
+        
+        //checking if the product exists  
+        if(!product){
+            return res.status(400).json({message:"product does not exist in this inventory please add as new product "});
+        }
+
+        product.Quantity = product.Quantity + body.Quantity;
+        await InventoryRepository.update(product.PrimiaryCol,product);
+        return res.status(200).json({message:"Inventory updated successfully",product:product})
+        
+    
+    }catch(err){
+        return res.status(500).json({message:err.message});
+    }
+});
+
+
+// Deduct the recieved quantity of the product from the given Inventory No 
+/* Request body:
+    Product name
+    Product quantity
+    Inventory No
+
+**jwt token (in header)
+*/
+router.put("/checkout",checkjwt,async (req:ARequest,res:Response)=>{
+try{
+    const body:InventoryDto = req.body;
+    const user:User = await Userrepository.findOne({where:{id:req.user.id}});
+
+    //checking if the user exists  
+    if(!user){
+        return res.status(400).json({message:"user not found!!"});
+    }
+
+    const product:Inventory = await InventoryRepository.findOne({where:{UserId:user.id,ProductName:body.ProductName,InventoryNumber:body.InventoryNumber}});
+    
+    //checking if the product exists  
+    if(!product){
+        return res.status(400).json({message:"product does not exist in this inventory please add as new product "});
+    }
+
+    if(body.Quantity>product.Quantity){
+        return res.status(400).json({message:"There isn't enough Product Quantity to checkout"});
+    }
+
+    product.Quantity = product.Quantity - body.Quantity;
+    await InventoryRepository.update(product.PrimiaryCol,product);
+    return res.status(200).json({message:"Inventory updated successfully",product:product})
+    
+
+}catch(err){
+    return res.status(500).json({message:err.message});
+}
+});
+
+
+//Remove a Product completely from inventory
+/* Request body:
+    Product name
+    Inventory No
+
+**jwt token (in header)
+*/
+router.put("/remove",checkjwt,async (req:ARequest,res:Response)=>{
+    try{
+        const body:InventoryDto = req.body;
+        const user:User = await Userrepository.findOne({where:{id:req.user.id}});
+    
+        //checking if the user exists  
+        if(!user){
+            return res.status(400).json({message:"user not found!!"});
+        }
+    
+        const product:Inventory = await InventoryRepository.findOne({where:{UserId:user.id,ProductName:body.ProductName,InventoryNumber:body.InventoryNumber}});
+        
+        //checking if the product exists  
+        if(!product){
+            return res.status(400).json({message:"product does not exist in this inventory"});
+        }
+       
+        await InventoryRepository.delete(product.PrimiaryCol);
+        return res.status(200).json({message:"Product deleted from Inventory successfully"})
+        
+    
+    }catch(err){
+        return res.status(500).json({message:err.message});
+    }
+    });
 
 
 module.exports = router;
